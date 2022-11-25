8d4eb73cc73240a9281436411d88bfc74da67fe9
Ricky TI <tobaracing1@gmail.com>
2022-11-26 00:23:11 +0700

    FIX navbar size and clickable

--- Profile/profile.js
+++ Profile/profile.js
@@ -6,27 +6,11 @@ function fnAction(){
     event.preventDefault();
 }
 
-function open_close_nav(){
-    var nav = document.getElementById("nav");
-    var check = document.getElementById("check-nav");
-    console.log("NYALA");
-    if(!check.checked){
-        check.checked = true;
-        nav.style.animationName = "movedown";
-    }
-    else{
-        check.checked = false;
-        nav.style.animationName = "moveup";
-    }
-}
-
 function open_form(){
     selectForm[0].style.display="flex";
 }
 
-function pageHome(){
-    windows.location.href = "./home";
-}
+
 
 
 
---
+++ topbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- TOP BAR -->
+    <div class="top-bar">
+        <div class="top-bar-left">
+            <button class="back"><img src="./Image/Arrow.png" alt=""></button>
+            <div class="top-bar-menu-name">Profile</div>
+        </div>
+        <div class="top-bar-right">
+            <button class="notif"><img src="./Image/notif.png" alt=""></button>
+            <button class="setting"><img src="./Image/setting.png" alt=""></button>
+        </div>
+    </div>
+</body>
+</html>
