d318a4bd0046b9260d103dff58ca30297ebcef10
Ricky TI <tobaracing1@gmail.com>
2022-12-07 19:58:30 +0700

    Push Code Elbert (Still error)

---
+++ addcourse/addcourse.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="addcourse.css" />
+    <link rel="stylesheet" href="../global.css" />
+    <script src="../global.js"></script>
+    <title>Course</title>
+  </head>
+  <body>
+    <?php include '../topbar.php' ?>
+    <div class="container">
+      <img src="../image/astronaut.png" alt="astronaut" />
+      <div class="cheer-word">
+        <p>
+          Help others and change lives -- including your own -- in your own way!
+        </p>
+      </div>
+      <p class="upload-course">Add Course</p>
+    </div>
+    <?php include '../nav.php'?>
+  </body>
+</html>
---
+++ social/social.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="social.css" />
+    <link rel="stylesheet" href="../global.css" />
+    <title>Socials</title>
+    <script src="../global.js"></script>
+  </head>
+  <body>
+    <?php include '../topbar.php' ?>
+    <div class="profile">
+      <div class="profile-picture">
+        <img src="../image/Intersect.png" alt="" />
+      </div>
+      <div class="profile-info">
+        <h3>Richard Hanitio</h3>
+        <p>Pirates</p>
+      </div>
+    </div>
+    <div class="chat-info">
+      <p>Groups</p>
+      <p>Friends</p>
+      <p>Chat</p>
+    </div>
+    <hr />
+    <div class="group-box">
+      <img src="../image/Group 167.png" alt="" />
+      <p>Create New Group</p>
+    </div>
+    <div class="group-box">
+      <img src="../image/Group 168.png" alt="" />
+      <p>Join Open Discussion</p>
+    </div>
+    <div class="group-box">
+      <img src="../image/Group 169.png" alt="" />
+      <p>Join A Group</p>
+    </div>
+    <br />
+    <br />
+    <div class="group-box">
+      <img src="../image/Group 167.png" alt="" />
+      <p>Group A</p>
+    </div>
+    <div class="group-box">
+      <img src="../image/Group 167.png" alt="" />
+      <p>Group A</p>
+    </div>
+    <div class="group-box">
+      <img src="../image/Group 167.png" alt="" />
+      <p>Group A</p>
+    </div>
+    <div class="group-box">
+      <img src="../image/Group 167.png" alt="" />
+      <p>Group A</p>
+    </div>
+    <?php include '../nav.php'?>
+  </body>
+</html>
